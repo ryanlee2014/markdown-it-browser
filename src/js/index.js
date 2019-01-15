521d73924975c925787f6398eabb144e2923585f
Ryan Lee <gxlhybh@gmail.com>
2019-01-15 21:32:52 +0800

    Add markdown photo preview API

--- src/js/index.js
+++ src/js/index.js
@@ -9,17 +9,19 @@ const mh = require("markdown-it-highlightjs");
 const mk = require("@ryanlee2014/markdown-it-katex");
 const mc = require("./markdonw-it-table-contents");
 const ma = require("markdown-it-anchor");
+const mi = require("markdown-it-images-preview");
 md.use(mk);
 md.use(mh);
 md.use(ma);
-md.use(mc, {includeLevel: [1,2], listType: "div class='ui bulleted link list'"});
+md.use(mc, {includeLevel: [1, 2], listType: "div class='ui bulleted link list'"});
+md.use(mi);
 
 const markdownPack = (html) => {
     return `<div class="markdown-body">${html}</div>`;
 };
 
 const preToSegment = (html) => {
-    return html.replace(/<pre>[\s\S]+?<\/pre>/g,`<div class='ui segment'>
+    return html.replace(/<pre>[\s\S]+?<\/pre>/g, `<div class='ui segment'>
     <div class="ui top attached label"><a class="copy context">Copy</a></div>$&</div>`)
 };
 
