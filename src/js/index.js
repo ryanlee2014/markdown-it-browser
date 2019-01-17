37e64078ed2e2fef3ddbcecbcd8502a2b8048243
Ryan Lee <gxlhybh@gmail.com>
2019-01-17 16:22:49 +0800

    Add markdownIt photo support for CUP Online Judge

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,43 @@
 import css from "../css/github-markdown.css"
 import css2 from "../css/github.min.css"
+function Instance(key = "", problem_id = "") {
+    const md = require("markdown-it")({
+        html: true,
+        breaks: true
+    });
+    const mh = require("markdown-it-highlightjs");
+    const mk = require("@ryanlee2014/markdown-it-katex");
+    const mc = require("./markdonw-it-table-contents");
+    const ma = require("markdown-it-anchor");
+    const mi = require("./markdown-it-images-preview");
+    md.use(mk);
+    md.use(mh);
+    md.use(ma);
+    md.use(mc, {includeLevel: [1, 2], listType: "div class='ui bulleted link list'"});
+    md.use(mi);
 
-const md = require("markdown-it")({
-    html: true,
-    breaks: true
-});
-const mh = require("markdown-it-highlightjs");
-const mk = require("@ryanlee2014/markdown-it-katex");
-const mc = require("./markdonw-it-table-contents");
-const ma = require("markdown-it-anchor");
-const mi = require("markdown-it-images-preview");
-md.use(mk);
-md.use(mh);
-md.use(ma);
-md.use(mc, {includeLevel: [1, 2], listType: "div class='ui bulleted link list'"});
-md.use(mi);
+    const markdownPack = (html) => {
+        return `<div class="markdown-body">${html}</div>`;
+    };
 
-const markdownPack = (html) => {
-    return `<div class="markdown-body">${html}</div>`;
-};
-
-const preToSegment = (html) => {
-    return html.replace(/<pre>[\s\S]+?<\/pre>/g, `<div class='ui segment'>
+    const preToSegment = (html) => {
+        return html.replace(/<pre>[\s\S]+?<\/pre>/g, `<div class='ui segment'>
     <div class="ui top attached label"><a class="copy context">Copy</a></div>$&</div>`)
-};
+    };
 
-const _render = md.render;
+    const _render = md.render;
 
-md.render = function () {
-    return markdownPack(preToSegment(_render.apply(md, arguments)));
-};
+    md.render = function () {
+        return markdownPack(preToSegment(_render.apply(md, arguments)));
+    };
 
-md.renderRaw = function () {
-    return preToSegment(md.renderInline(...arguments));
-};
+    md.renderRaw = function () {
+        return preToSegment(md.renderInline(...arguments));
+    };
 
+    return Object.assign(md, {key, problem_id});
+}
+
+const md = Instance();
+md.newInstance = Instance;
 module.exports = md;
---
+++ src/js/markdown-it-images-preview.js
@@ -0,0 +1,46 @@
+/**
+ * @Author: HuaChao Chen <chc>
+ * @Date:   2017-06-12T21:06:58+08:00
+ * @Email:  chenhuachaoxyz@gmail.com
+ * @Filename: index.js
+ * @Last modified by:   chc
+ * @Last modified time: 2017-06-12T21:18:15+08:00
+ * @License: MIT
+ * @Copyright: 2017
+ */
+
+module.exports = function (md, config) {
+    md.image_add = function (src, data) {
+        if (!(md.__image instanceof Object))
+            md.__image = {}
+        md.__image[src] = data;
+    }
+    md.image_del = function (src) {
+        if (!(md.__image instanceof Object))
+            md.__image = {}
+        delete md.__image[src];
+    }
+    var imagedefault = md.renderer.rules.image;
+    md.renderer.rules.image = function (tokens, idx, options, env, slf) {
+        var _attrs = tokens[idx].attrs;
+        console.log(_attrs);
+        console.log(md);
+        //if(md.__image instanceof Object){
+        for (var i = 0; i < _attrs.length; i++) {
+            var validate = false;
+            if (!isNaN(_attrs[i][1])) {
+                validate = true;
+            }
+
+            if (_attrs[i][0] === 'src' /*&& md.__image.hasOwnProperty(tokens[idx].attrs[i][1])*/ && validate === true) {
+                _attrs.push(['rel', _attrs[i][1]]);
+                //_attrs[i][1] = md.__image[tokens[idx].attrs[i][1]]
+                _attrs[i][1] = "/images/" + md.problem_id + "/" + md.key + "/" + _attrs[i][1] + ".jpg";
+                console.log(_attrs[i]);
+                break;
+            }
+        }
+        //}
+        return imagedefault(tokens, idx, options, env, slf);
+    }
+}
